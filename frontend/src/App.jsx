139a29edf4f306aeb7964dc59c9bdcba547df00a
karnking <karankotai9183@gmail.com>
2023-11-27 22:02:57 +0530

    made basic components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import AllRoutes from './components/AllRoutes'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <AllRoutes />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
